ab83fac91b4600b7e52d744a38bac550c7d39b96
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-02-07 12:50:01 +0000

    Added existing files to repository

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import CitySearch from "./CitySearch";
+import DailyOverview from "./DailyOverview";
+import WeeklyForecast from "./WeeklyForecast";
+import background from "./images/sky1.jpg";
+
+export default function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <CitySearch />
+            <DailyOverview />
+            <WeeklyForecast />
+          </div>
+        </div>
+      </div>
+      <div>
+        <span className="github-link">
+          <a href="/" target="_blank" className="link" rel="noreferrer">
+            Open-source code
+          </a>
+          <span className="creator-name"> by Laura Petrie</span>
+        </span>
+        <script src="src/index.js"></script>
+      </div>
+    </div>
+  );
+}
---
+++ src/WeeklyForecast.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function WeeklyForecast() {
+  return (
+    <div className="WeeklyForecast">
+      <div className="weather-forecast" id="weather-forecast">
+        Insert forecast here
+      </div>
+    </div>
+  );
+}
