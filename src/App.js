b8681425965c00a9f779c648a7003b904f77cf12
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-02-20 11:43:56 +0000

    Installed bootstrap from npm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+
 import CitySearch from "./CitySearch";
 import DailyOverview from "./DailyOverview";
 import WeeklyForecast from "./WeeklyForecast";
