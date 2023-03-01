f98387360816fb95704f8544f55bb2fc74bdf262
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-03-01 11:30:15 +0000

    Included useEffect for weekly forecast

--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeeklyForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -7,9 +7,22 @@ export default function WeeklyForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setLoaded(true);
     setForecast(response.data.daily);
+  }
+
+  function getWeather() {
+    const apiKey = "e7cba0f4344b9ae720f19t5d48co46c3";
+    let latitude = props.coordinates.latitude;
+    let longitude = props.coordinates.longitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (loaded) {
@@ -17,25 +30,20 @@ export default function WeeklyForecast(props) {
       <div className="WeeklyForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index > 1 && index < 7) {
+            if (index > 0 && index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
             }
+            return null;
           })}
         </div>
       </div>
     );
   } else {
-    const apiKey = "e7cba0f4344b9ae720f19t5d48co46c3";
-    let latitude = props.coordinates.latitude;
-    let longitude = props.coordinates.longitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
+    getWeather();
     return null;
   }
 }
